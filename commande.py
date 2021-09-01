8215acbe6994abe856dba2427794778e0a0c35bd
JALIL KHALIL <khaliljalil0@gmail.com>
2021-09-01 01:25:06 +0100

    'jalil'

--- commande.py
+++ commande.py
@@ -1,8 +1,7 @@
 import subprocess
 
 def githubcommande():
-    my_file = open("com.txt", "r")
-    print(my_file.read())
+
     subprocess.call("git add .")
     subprocess.call("git commit -m 'jalil'")
     subprocess.call("git push")
