bac7ebfceaa20212d7340d4c922440a3c33bd6a5
JALIL KHALIL <khaliljalil0@gmail.com>
2021-09-01 01:12:48 +0100

    'jalil'

---
+++ commande.py
@@ -0,0 +1,14 @@
+
+
+def githubcommande():
+    with open('C:/Users/T460/Desktop/api/com.txt', 'r') as f:
+        f.read()
+        print(f.read())
+
+def a():
+    my_file = open("com.txt", "r")
+    print(my_file.read())
+
+
+
+a()
